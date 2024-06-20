a54a2ae60781054467e2c086d8a31760c5a17465
merfan-bagheri <merfan.bagheri00@gmail.com>
2024-06-20 18:14:22 +0330

    complete the question 2

--- HW2/q2/cli.sh
+++ HW2/q2/cli.sh
@@ -11,18 +11,31 @@ mkdir -p $rootfs_path
 # Use debootstrap to install Ubuntu 20.04 filesystem into the container root filesystem
 debootstrap --variant=minbase focal $rootfs_path
 
-mkdir -p /sys/fs/cgroup/memory/mycontainer;
-echo $((memory_limit_MB * 1024 * 1024)) > /sys/fs/cgroup/memory/mycontainer/memory.limit_in_bytes
-echo $((memory_limit_MB * 1024 * 1024)) > /sys/fs/cgroup/memory/mycontainer/memory.memsw.limit_in_bytes
-echo $$ > /sys/fs/cgroup/memory/mycontainer/tasks
+# Set memory limit if provided, otherwise don't limit the memory(limit the memory usage for the parent of my container)
+if [ -n "$memory_limit_MB" ]; then
+    echo "Memory limit set to $memory_limit_MB MB"
+    mkdir -p /sys/fs/cgroup/memory/mycontainer
+    echo $((memory_limit_MB * 1024 * 1024)) > /sys/fs/cgroup/memory/mycontainer/memory.limit_in_bytes
+    echo $((memory_limit_MB * 1024 * 1024)) > /sys/fs/cgroup/memory/mycontainer/memory.memsw.limit_in_bytes
+    echo $$ > /sys/fs/cgroup/memory/mycontainer/tasks
+else
+    echo "Memory limit not set, not limiting the memory"
+fi
 
 # Use unshare to create new namespaces for the container
 unshare --uts --ipc --net --pid --mount --fork chroot $rootfs_path /bin/bash -c "
   mount -t proc proc /proc;
   hostname $hostname;
-  mkdir -p /sys/fs/cgroup/memory/mycontainer;
-  echo $((memory_limit_MB * 1024 * 1024)) > /sys/fs/cgroup/memory/mycontainer/memory.limit_in_bytes;
-  echo $((memory_limit_MB * 1024 * 1024)) > /sys/fs/cgroup/memory/mycontainer/memory.memsw.limit_in_bytes;
-  echo $$ > /sys/fs/cgroup/memory/mycontainer/tasks;
-  clear;
   exec bash"
+
+
+# set limitation on container new filesystem
+# unshare --uts --ipc --net --pid --mount --fork chroot $rootfs_path /bin/bash -c "
+#   mount -t proc proc /proc;
+#   hostname $hostname;
+#   mkdir -p /sys/fs/cgroup/memory/mycontainer;
+#   echo $((memory_limit_MB * 1024 * 1024)) > /sys/fs/cgroup/memory/mycontainer/memory.limit_in_bytes;
+#   echo $((memory_limit_MB * 1024 * 1024)) > /sys/fs/cgroup/memory/mycontainer/memory.memsw.limit_in_bytes;
+#   echo $$ > /sys/fs/cgroup/memory/mycontainer/tasks;
+#   clear;
+#   exec bash"
